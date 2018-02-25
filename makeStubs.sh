29e8abca2513d1a633423684b0546b021f84efa3
Robert Adams <misterblue@misterblue.com>
2018-02-24 16:04:12 -0800

    Add transport block to Basil messages. For the moment, I'll role my own simple transport and RPC mechanism. Someday will be replaced by some real library.

--- makeStubs.sh
+++ makeStubs.sh
@@ -8,35 +8,44 @@ PROTOBUF=/home/basil/protobuf
 
 PROTOC=${PROTOBUF}/src/protoc
 
+EXTRAJSOUT=import_style=commonjs,binary:
+
 # Do a generation for $1=protoFile, $2=langTarget, $3=targetDir
 function doGen() {
     # echo "doGen  1=$1, 2=$2, 3=$3"
     SRC=protocol/${1}.proto
     if [[ -e "$SRC" ]] ; then
         mkdir -p "$3"
-        ${PROTOC} --${2}_out=$3 --proto_path=protocol/ "$SRC"
+        OUTPARAM=--${2}_out=${3}
+        if [[ "$2" == "js" ]] ; then
+            OUTPARAM=--js_out=import_style=commonjs,binary:${3}
+        fi
+        ${PROTOC} ${OUTPARAM} --proto_path=protocol/ "$SRC"
     else
         echo "No source file for $SRC"
     fi
 }
 
 # Base structs and tables
-doGen BasilTypes js             gen-BasilTypes-js
 doGen BasilTypes cpp            gen-BasilTypes-cpp
 doGen BasilTypes csharp         gen-BasilTypes-cs
 doGen BasilTypes java           gen-BasilTypes-java
 
+# Message transport
+doGen BTransport cpp            gen-BasilTransport-cpp
+doGen BTransport csharp         gen-BasilTransport-cs
+doGen BTransport java           gen-BasilTransport-java
+
 # Basil Server -- for talking to the Basil server
-doGen BasilServer js            gen-BasilServer-js
 doGen BasilServer cpp           gen-BasilServer-cpp
 doGen BasilServer csharp        gen-BasilServer-cs
 doGen BasilServer java          gen-BasilServer-java
 
 # Pesto Server
-doGen PestoServer js            gen-PestoServer-js
+doGen PestoServer go            gen-PestoServer-go
+doGen PestoServer csharp        gen-PestoServer-cs
 
 # Pesto Client
-doGen PestoClient js            gen-PestoClient-js
 doGen PestoClient cpp           gen-PestoClient-cpp
 doGen PestoClient csharp        gen-PestoClient-cs
 doGen PestoClient java          gen-PestoClient-java
